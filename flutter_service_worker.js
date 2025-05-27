1dda799b870a6084e6166c228b973714851cac46
nitinnaikwadi1 <nitinnaikwadi1@gmail.com>
2025-05-27 12:12:37 +0530

    test

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -82,7 +82,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "d122b11e3a6a02803af1a6f2c2a506e1
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "fe7fad398bfc523ea059d5197dbb1ddb",
+"flutter_bootstrap.js": "34b0d7a80751bf8f764a1f6eeff14eff",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
